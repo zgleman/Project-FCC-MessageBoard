1f6d2c8b0dea644fa9cbc1428217d2a7e52b1ce7
Glitch (zgleman-messageboard) <none>
2019-10-28 10:21:41 +0000

    😜📫 Checkpoint ./routes/api.js:7900686/782

--- routes/api.js
+++ routes/api.js
@@ -16,9 +16,28 @@ module.exports = function (app) {
   
   app.route('/api/threads/:board')
      .post(function(req, res){
+    var board = req.params.board;
+    var text = req.body.text;
+    var delete_password = req.body.delete_password;
+    var obj = threadHandler.create(board, text, delete_password);
     
+    res.redirect('/api/threads/' + obj.board);
   })
-  
+    .get(function(req, res){
+    var board = req.params.board;
+    var obj = threadHandler.list(board);
+    res.json({obj});
+  })
+  .delete(function(req, res){
+    var thread_id = req.body.thread_id;
+    var delete_password = req.body.delete_password;
+    var answer = threadHandler.delete(thread_id, delete_password);
+    res.send(answer);
+  })
+  .put(function(req, res){
+    var thread_id = req.body.thread_id;
+    var answer = threadHandler.report()
+  })
   
   app.route('/api/replies/:board');
 

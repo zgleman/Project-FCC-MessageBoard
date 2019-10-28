0f7202c565cf560972d885802a6023d2aa7644d1
Glitch (zgleman-messageboard) <none>
2019-10-28 09:11:34 +0000

    🐲💐 Checkpoint ./controller/threads.js:7900686/421 ./package.json:7900686/26

--- controller/threads.js
+++ controller/threads.js
@@ -1,2 +1,17 @@
 var mongoose = require('mongoose');
-mongoose.connect(process)
+mongoose.connect(process.env.DB, {useNewUrlParser: true});
+const shortid = require('shortid');
+
+const thread = mongoose.model('thread', {
+  _id: {'type': String,
+        'default': shortid.generate},
+  board: String,
+  thread: String,
+  password: String,
+  reported: Boolean,
+  replies:[{ 
+            time: Date,
+            message: String
+  }]
+});
+
